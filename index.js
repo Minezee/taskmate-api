b1f2428eea86b9fd49d5cd8406b8d179c79deabd
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-01-24 22:44:29 +0700

    remove cors

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors({
-origin: 'http://localhost:3000',
-credentials: true,
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Access-Control-Allow-Origin'],
 }));
 
 app.use(bodyParser.json())
